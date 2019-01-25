25c0abefafa2977619f36c8fcd39106cad780550
basyri <husain.basyri@gmail.com>
2019-01-25 08:01:08 +0700

    update

--- exercises13.js
+++ exercises13.js
@@ -41,7 +41,11 @@ function getTotal(arrNumber) {
 function mostFrequentLargestNumbers(arrNumber) {
     var listSort = sorting(arrNumber);
     var countHighest = getTotal(listSort);
-    return countHighest;
+    if (arrNumber.length === 0) {
+        return ''
+    } else {
+        return 'angka paling besar adalah ' + listSort[0] + ' dan jumlah kemunculan sebanyak ' + countHighest + ' kali';
+    }
 }
 
 console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
